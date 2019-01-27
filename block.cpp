26c1e304c1d67036c9e72eab5eda8d6e6ff3ad6a
Geovane Marquez <geovane@csil-11.cs.ucsb.edu>
2019-01-27 11:46:28 -0800

    Add new files

--- block.cpp
+++ block.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	int var1, var2;
-	cout << "Enter number of rows and columns:";
+	cout << "Enter number of rows and columns:" << endl;
 	cin >> var1 >> var2;
 	while(var1 > 0 && var2 > 0)
 	{
