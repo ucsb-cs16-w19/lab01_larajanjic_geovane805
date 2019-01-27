f31e26f4496e28744b9c8f77cdc7a91323dea1e3
Geovane Marquez <geovane@csil-11.cs.ucsb.edu>
2019-01-27 11:34:52 -0800

    Add the new files

--- block.cpp
+++ block.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
 	int var1, var2;
-	cout << "Enter the number of rows and columns:" << endl;
-	cin >> var1 >> var2;
 	do {
+		cout << "Enter the number of rows and columns:\n";
+		cin >> var1 >> var2;
 		for (int i = 0; i < var1; i++) 
 		{
 			for (int j = 0; j < var2; j++) 
@@ -16,7 +16,7 @@ int main() {
 		}
 		cout << "Enter the number of rows and columns:" << endl;
 		cin >> var1 >> var2;
-	   }while(var1 != 0 && var2 != 0);
+	   }while(var1 > 0 && var2 > 0);
 
 	return 0;
 }
